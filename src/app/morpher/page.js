02e596c63ab52e48cecaa81072317ea3aa232a98
jaivanshchawla <jaivansh.chawla@gmail.com>
2026-03-05 12:53:26 +0530

    morph hero

--- src/app/morpher/page.js
+++ src/app/morpher/page.js
@@ -1,4 +1,9 @@
-export default function MorpherPage() {
-  return <main className="h-screen w-screen" />;
-}
+import IntroAnimation from "@/components/scroll-morph-hero";
 
+export default function MorpherPage() {
+  return (
+    <main className="h-screen w-screen overflow-hidden">
+      <IntroAnimation />
+    </main>
+  );
+}
